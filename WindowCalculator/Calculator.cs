19d31c85de94e78e6e35e6cd6e7617e495aa1d56
nhenriken <118528844+nhenriken@users.noreply.github.com>
2023-03-23 16:24:17 +0100

    OS injection vulnerable Windows command line calculator

---
+++ WindowCalculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace WindowCalculator
+{
+    public static class Calculator
+    {
+        public static string Calculate(string number1, string number2)
+        {
+            var argument = $"/C set /A {number1}+{number2}";
+            var result = string.Empty;
+
+            using (Process myProcess = new Process())
+            {
+                myProcess.StartInfo.FileName = "cmd.exe";
+                myProcess.StartInfo.Arguments = argument;
+                myProcess.StartInfo.RedirectStandardOutput = true;
+                myProcess.Start();
+
+                result = myProcess.StandardOutput.ReadToEnd();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ WindowCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WindowCalculator;
+
+Console.WriteLine("Welcome to the Window Calculator!");
+Console.WriteLine();
+Console.WriteLine("The Window Calculator helps you sum the number of windows on two buildings.");
+Console.WriteLine();
+
+while (true)
+{
+    Console.Write("Write the number of windows on the first building: ");
+    string? number1 = Console.ReadLine();
+    Console.Write("Write the number of windows on the second building: ");
+    string? number2 = Console.ReadLine();
+    Console.WriteLine();
+
+    var result = Calculator.Calculate(number1, number2);
+
+    Console.Write($"The total number of windows on the buildings are: ");
+    Console.WriteLine(result);
+    Console.WriteLine();
+    Console.WriteLine();
+}
